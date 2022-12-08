4d470abe6dfd1f829f974ad1da28907e57951f09
Cindy <cho102@ucr.edu>
2022-12-08 11:13:32 -0800

    finished

--- final.py
+++ final.py
@@ -1,6 +1,8 @@
 import math
 from math import dist
 import copy
+from itertools import combinations
+import time
 
 data = []
 def printFeature(set):
@@ -86,53 +88,49 @@ def backwardElimination():
     for i in range(1, len(data[0]), 1):
         bestAcc = 0;
         worstAcc=100;
-        bestFeat = 0;
-        worstFeat = 0
+        bestFeatCombo = 0;
+        worstFeatCombo = 0;
+        worstFeat = 0;
         print("On level " + str(i) + " of the search tree")
-        for j in range(1, len(data[0]), 1): #iterate through the features
-            if 0:
-                continue;
-            else:
-                # print("before", bestSelection)
-                for q in range(len(removedFeature)):
-                    if removedFeature[q] in bestSelection:
-                        bestSelection.remove(removedFeature[q])
-                temp = copy.deepcopy(bestSelection)
-                # print("after", bestSelection)
-                # print("after", temp)
-                if j in removedFeature:
-                    continue;
-                temp.remove(j)
-                testAcc = leaveOneOutCrossVal(data, temp, j)
-                print("\tUsing feature(s) " + printFeature(temp) + " with {" + str(j) + "} accuracy is " + str(round(testAcc*100,2)) +"%")
-                if testAcc > bestAcc:
-                    bestAcc = testAcc
-                    bestFeat = j
-                if testAcc < worstAcc:
-                    worstAcc = testAcc
-                    worstFeat = j
+        temp=[]
+        temp += list(combinations(bestSelection, len(data[0])-i))
+        for j in range(len(temp)):
+            tempJ = list(copy.deepcopy(temp[j]))
+            val = tempJ.pop(0)
+            testAcc = leaveOneOutCrossVal(data, tempJ, val)
+            print("\tUsing feature(s) " + printFeature(temp[j]) + " accuracy is " + str(round(testAcc*100,2)) +"%")
+            if testAcc > bestAcc:
+                bestAcc = testAcc
+                bestFeatCombo = temp[j]
+            if testAcc < worstAcc:
+                worstAcc = testAcc
+                worstFeatCombo = temp[j]
         if warning == 0 and bestAcc < finalAcc:
             warning = 1;
             print("(WARNING: Accuracy has decreased! Continuing search in case of local maxima)")
         elif bestAcc > finalAcc:
             finalAcc = bestAcc
-            finalSelection.append(bestFeat)
-        
-        # bestSelection.append(bestFeat);
-        removedFeature.append(worstFeat)
-        print(removedFeature)
-        print("On level " + str(i) + ", feature set " + printFeature(bestSelection) + " was best, accuracy is "+str(round(bestAcc*100,2))+ "%\n")
+            finalSelection = bestFeatCombo
+        if bestFeatCombo != worstFeatCombo:
+            difference = list(set(worstFeatCombo).symmetric_difference(bestFeatCombo))
+            for q in range(len(difference)):
+                if difference[q] in worstFeatCombo:
+                    worstFeat = difference[q]
+            removedFeature.append(worstFeat)
+            bestSelection.remove(worstFeat)
+        print("On level " + str(i) + ", feature set " + printFeature(bestFeatCombo) + " was best, accuracy is "+str(round(bestAcc*100,2))+ "%")
+        if worstFeat!=0: print("On level " + str(i) + ", feature set with " + str(worstFeat) + " was the worst\n")
+        else: print("")
     print("Finished search!!! The best feature subset is " + printFeature(finalSelection) + ", which has an accuracy of " + str(round(finalAcc*100,2)) + "%")
-    
     return;
 
 #main program that runs
 def main():
     print("Welcome to the Feature Selection Algorithm");
-    # print("Choose the file to test:")
+    print("Choose the file to test:")
     # fullPath = input("File Input: ");
-    fullPath = '/Users/cindyho/Desktop/CS170/cs170_project2/CS170_Small_Data__1.txt' 
-    # fullPath = '/Users/cindyho/Desktop/CS170/cs170_project2/CS170_Large_Data__72.txt'
+    # fullPath = '/Users/cindyho/Desktop/CS170/cs170_project2/CS170_Small_Data__1.txt' 
+    fullPath = '/Users/cindyho/Desktop/CS170/cs170_project2/CS170_Large_Data__72.txt'
     
     with open(fullPath, 'r') as file:
         for line in file:
@@ -142,13 +140,17 @@ def main():
     print("\nThis dataset has " + str(len(data[0])-1) + " features (not including the class attribute), with " + str(len(data)) + " instances.\n")
 
     print("Type the number of the algorithm you want to run:\n1. Forward Selection\n2. Backward Elimination")
-    alg_input = 2#int(input("Algorithm Input: "))
-
+    alg_input = int(input("Algorithm Input: "))
+    start_time=time.time();
     if alg_input == 1:
         print("Forward Selection Chosen.\n")
         forwardSelection();
     else:
         print("Backward Elimination Chosen.\n")
         backwardElimination();
-
+    end_time = time.time() - start_time
+    if end_time <60:
+        print("Total time elapsed(in seconds): " + str(round(end_time, 1))+ "\n\n")
+    else:
+        print("Total time elapsed(in minutes): " + str(round(end_time/60, 1))+ "\n\n")
 main();
